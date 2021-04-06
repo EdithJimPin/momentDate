cac612c27d0eb8ab25a597c33c4a5a54e2ea04a9
EdithJimPin <56369614+EdithJimPin@users.noreply.github.com>
2021-04-06 15:30:14 -0500

    Fecha con formato Intl.DateTimeFormat

--- date/src/App.js
+++ date/src/App.js
@@ -8,7 +8,33 @@ import 'moment/locale/es';
 
 function App() {
  
-  const today = moment();
+  const today = moment();;
+  const date = new Date();
+  //Idioma, se cambia en el argumento
+  const fechaIdioma = new Intl.DateTimeFormat('es-MX').format(date); //06/4/21
+  //Formatos personalizados, usando options
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const fechaLarga = new Intl.DateTimeFormat('es-MX', options).format(date); //martes, 6 de abril de 2021
+  options = {
+    hour: 'numeric', minute: 'numeric', second: 'numeric',
+    timeZone: 'America/Mexico_City',
+    //timeZoneName: 'short'
+  };
+  const fechaNumerica = new Intl.DateTimeFormat('es-MX', options).format(date);
+  options = {
+    year: 'numeric', month: 'numeric', day: 'numeric',
+    hour: 'numeric', minute: 'numeric', second: 'numeric',
+    hour12: true,
+    timeZone: 'America/Mexico_City'
+  };
+  const fechaPm = new Intl.DateTimeFormat('es-MX', options).format(date);
+  options = {
+    year: 'numeric', month: 'numeric', day: 'numeric',
+    hour: 'numeric', minute: 'numeric', second: 'numeric',
+    hour12: false,
+    timeZone: 'America/Mexico_City'
+  };
+  const fecha24 = new Intl.DateTimeFormat('es-MX', options).format(date);
 
   return (
     <Fragment>
@@ -40,6 +66,27 @@ function App() {
           <Moment format="MMMM D YYYY, h:mm:ss a">
             {today}
         </Moment>
+      </label>
+      <br/>
+      <h2>Formato con Intl.DateTimeFormat</h2>
+      <label htmlFor="">
+          {fechaIdioma}
+      </label>
+      <br/>
+      <label htmlFor="">
+          {fechaLarga}
+      </label>
+      <br/>
+      <label htmlFor="">
+          {fechaNumerica}
+      </label>
+      <br/>
+      <label htmlFor="">
+          {fechaPm}
+      </label>
+      <br/>
+      <label htmlFor="">
+          {fecha24}
       </label>
 
     </Fragment>
